2534f14ac1a17c0f4cce9c4be8f0c5e873abb83c
Benoit Ripoche <benoit.ripoche@kiplin.com>
2022-10-03 18:49:47 +0200

    fix img download isNew

--- app.js
+++ app.js
@@ -1,48 +1,72 @@
 require('dotenv').config()
-var Twitter = require('twitter');
-var download = require('image-downloader')
-var mkdirp = require('mkdirp')
-var fs = require('fs')
+const Twitter = require('twitter');
+const download = require('image-downloader');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
 
 // Set twitter client
-var client = new Twitter({
-  consumer_key:process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key:process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 // Get keyword from command line
-var keyword = process.argv[2]
+const keyword = process.argv[2];
+
+// Colored console log
+const consoleLogYellow = function(string) {
+  console.log('\x1b[33m', string, '\x1b[0m')
+};
+const consoleLogRed = function(string) {
+  console.log('\x1b[31m', string, '\x1b[0m')
+};
+const consoleLogGreen = function(string) {
+  console.log('\x1b[32m', string, '\x1b[0m')
+};
+
+
+// Check if file is new
+const isNew = function(media_id, path) {
+  fs.readdirSync(path).forEach(file => {
+    var pattern = new RegExp(/([0-9]*)(-@)(.*)/gm)
+    var res = pattern.exec(file)
+    if (res[1] === media_id) {
+      return false;
+    }
+  })
+  return true;
+};
 
 // Send error message if keyword is undefined
-if (keyword == undefined) {
+if (keyword === undefined) {
   console.log('Please provide a search keyword. Use the command "npm start keyword"')
 }
 
 // Start search
 else {
-  console.log('########################################')
-  console.log(`# Looking for "${keyword}" images`)
-  console.log('########################################')
+  console.log('###########################################')
+  console.log(`# Looking for "${keyword}" images in tweets `)
+  console.log('###########################################')
 
   client.stream('statuses/filter', {track: keyword}, function(stream) {
     stream.on('data', function(event) {
-      var user_id = event.user.id
-      var user_name = event.user.screen_name
+      const user_id = event.user.id;
+      const user_name = event.user.screen_name;
       console.log('########################################')
       console.log(`New result found for keyword: ${keyword}`)
       console.log(`User ID: ${user_id}`)
       console.log(`User name: @${user_name}`)
 
       if (event && event.entities && event.entities.media) {
-        var media_url = event.entities.media[0].media_url
-        var media_id = event.entities.media[0].id_str
+        const media_url = event.entities.media[0].media_url;
+        const media_id = event.entities.media[0].id_str;
         consoleLogYellow('One media found :D')
         consoleLogYellow(`URL: ${media_url}`)
 
         // Set subfolder path where to save images
-        var path = `./downloaded_images/${keyword}`
+        const path = `./downloaded_images/${keyword}`;
 
         // Create subfolder if not exist
         mkdirp(path, function(err) {
@@ -50,10 +74,10 @@ else {
             console.error(err)
           } else {
             // Set download options
-            var options = {
+            const options = {
               url: media_url,
-              dest: `${path}/${media_id}-@${user_name}-(id:${user_id}).jpg`
-            }
+              dest: `${path}/${media_id}-@${user_name}-(id:${user_id}).jpg`,
+            };
 
             // Save image if new
             if (isNew(media_id, path)) {
@@ -79,25 +103,4 @@ else {
       console.log(`Error: ${error}`);
     });
   });
-}
-
-
-// Colored console log
-var consoleLogYellow = function(string) {
-  console.log('\x1b[33m', string, '\x1b[0m')
-}
-var consoleLogRed = function(string) {
-  console.log('\x1b[31m', string, '\x1b[0m')
-}
-var consoleLogGreen = function(string) {
-  console.log('\x1b[32m', string, '\x1b[0m')
-}
-
-// Check if file is new
-var isNew = function(media_id, path) {
-  fs.readdirSync(path).forEach(file => {
-    var pattern = new RegExp(/([0-9]*)(-@)(.*)/gm)
-    var res = pattern.exec(file)
-    return res[1] != media_id;
-  })
 }
